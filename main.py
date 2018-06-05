6b0bc3eaf7610b339555d45ddcff346f81a2f6f5
Luke Cahill <lmc@outlook.com>
2018-06-05 21:13:36 +0100

    Add way to quit game

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from components.board import Board
 from components.user_input import UserInput
 
 def main():
-    crossword = Board(5, 5)
+    crossword = Board(10, 10)
     user_input = UserInput()
     crossword.read_clues("questions.csv")
     crossword.print_board(True)
@@ -10,6 +10,8 @@ def main():
     
     while True:
         guessed = user_input.user_guess_word()
+        if guessed == ":q":
+            break
         crossword.guess_word(guessed)
         crossword.print_board(True)
 
