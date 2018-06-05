6bdf8cb67e557794a7a62fd56badb26cafd6547b
Luke Cahill <lmc@outlook.com>
2018-06-05 21:05:41 +0100

    Allow the user to actually guess the word using new user input functionality

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from components.board import Board
+from components.user_input import UserInput
 
 def main():
     crossword = Board(5, 5)
+    user_input = UserInput()
     crossword.read_clues("questions.csv")
     crossword.print_board(True)
     crossword.print_clues()
+    
+    while True:
+        guessed = user_input.user_guess_word()
+        crossword.guess_word(guessed)
+        crossword.print_board(True)
 
 if __name__ == "__main__":
     main()
